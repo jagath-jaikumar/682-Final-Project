6da48c88c801dd4dc3ff45b43893a098df8d3ad9
jagath-jaikumar <jjaikumar@wpi.edu>
2019-10-30 17:28:16 -0400

    classifier almost working

---
+++ ClassifierNet/predict.py
@@ -0,0 +1,81 @@
+import glob
+import pickle
+import numpy
+import pandas as pd
+from music21 import converter, instrument, note, chord
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Activation
+from tensorflow.keras import utils as np_utils
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+
+sequence_length = 100
+
+
+def generate(filename):
+    network_input = get_notes(filename)
+    model = create_network(network_input)
+    label = prediction(model, network_input)
+
+def get_notes(filename):
+
+    midi = converter.parse(filename)
+    notes = []
+    
+
+    network_input = []
+
+    print("Parsing %s" % filename)
+
+    notes_to_parse = None
+
+    try:
+        s2 = instrument.partitionByInstrument(midi)
+        notes_to_parse = s2.parts[0].recurse()
+    except:
+        notes_to_parse = midi.flat.notes
+
+    for element in notes_to_parse:
+        if isinstance(element, note.Note):
+            notes.append(str(element.pitch))
+        elif isinstance(element, chord.Chord):
+            notes.append('.'.join(str(n) for n in element.normalOrder))
+
+    pitchnames = sorted(set(item for item in notes))
+
+    note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
+
+    for i in range(0, len(notes)-sequence_length, 1):
+        sequence_in = notes[i:i + sequence_length]
+        network_input.append([note_to_int[char] for char in sequence_in])
+
+    n_patterns = len(network_input)
+
+    network_input = numpy.reshape(network_input, (n_patterns, sequence_length))
+    network_input = network_input / float(len(notes))
+
+    return network_input[0], pitchnames
+
+def create_network(network_input):
+    """ create the structure of the neural network """
+    model = Sequential()
+    model.add(Dense(128, activation='relu',input_dim=network_input[0]))
+    model.add(Dense(10, activation='softmax'))
+    model.add(Dense(1))
+    model.compile(loss='binary_crossentropy', optimizer='rmsprop')
+
+    model.load_weights('classification-improvement-10.hdf5')
+
+    return model
+
+
+def prediction(model, network_input):
+    label = model.predict(network_input)
+    print(label)
+
+
+if __name__ == '__main__':
+    filename = "midi_songs/8.mid"
+    generate(filename)
--- ClassifierNet/train.py
+++ ClassifierNet/train.py
@@ -1,6 +1,7 @@
 import glob
 import pickle
 import numpy
+import pandas as pd
 from music21 import converter, instrument, note, chord
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
@@ -9,27 +10,31 @@ from tensorflow.keras.layers import Activation
 from tensorflow.keras import utils as np_utils
 from tensorflow.keras.callbacks import ModelCheckpoint
 
+
+sequence_length = 100
+
 def train_network():
-    notes = get_notes()
-    n_vocab = len(set(notes))
-    network_input, network_output = prepare_sequences(notes, n_vocab)
-    model = create_network(network_input, n_vocab)
+    network_input, network_output = get_notes()
+    model = create_network(network_input)
     train(model, network_input, network_output)
+
+
 
 def get_notes():
     data = pd.read_csv("song_category.csv")
 
     dic = data.set_index('song_title').T.to_dict('list')
 
-    category = dic[song_name][0]
-
-
-
     notes = []
     
+    network_input = []
+    network_output = []
 
-    for file in glob.glob("midi_songs/*.mid"):
+    num_notes = 0
+
+    for file in glob.glob("midi_songs/training_subset/*.mid"):
         midi = converter.parse(file)
+        song_name = str(file).split("/")[-1]
 
         print("Parsing %s" % file)
 
@@ -47,51 +52,51 @@ def get_notes():
             elif isinstance(element, chord.Chord):
                 notes.append('.'.join(str(n) for n in element.normalOrder))
 
-    with open('data/notes', 'wb') as filepath:
-        pickle.dump(notes, filepath)
+        pitchnames = sorted(set(item for item in notes))
 
-    return notes
-
-def prepare_sequences(notes, n_vocab):
+        note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
 
 
+        category = dic[song_name][0]
 
 
-    sequence_length = 100
-    pitchnames = sorted(set(item for item in notes))
 
-    note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
-
-    network_input = []
-    network_output = []
-
-    for i in range(0, len(notes) - sequence_length, 1):
-        sequence_in = notes[i:i + sequence_length]
-        sequence_out = notes[i + sequence_length]
-        network_input.append([note_to_int[char] for char in sequence_in])
-        network_output.append(note_to_int[sequence_out])
+        for i in range(0, len(notes) - sequence_length, 1):
+            sequence_in = notes[i:i + sequence_length]
+            if category == 'dark':
+                network_output.append(0)
+            else:
+                network_output.append(1)
+            network_input.append([note_to_int[char] for char in sequence_in])
+        num_notes += len(notes)
 
     n_patterns = len(network_input)
 
-    network_input = numpy.reshape(network_input, (n_patterns, sequence_length, 1))
-    network_input = network_input / float(n_vocab)
+    network_input = numpy.reshape(network_input, (n_patterns, sequence_length))
+
+    network_input = network_input / float(num_notes)
 
     network_output = np_utils.to_categorical(network_output)
 
     return (network_input, network_output)
 
-def create_network(network_input, n_vocab):
+
+
+def create_network(network_input):
     """ create the structure of the neural network """
     model = Sequential()
     model.add(Dense(128, activation='relu'))
     model.add(Dense(10, activation='softmax'))
+    model.add(Dense(1))
+    model.compile(loss='binary_crossentropy', optimizer='rmsprop')
+
 
     return model
 
 def train(model, network_input, network_output):
     """ train the neural network """
     filepath = "classification-improvement-{epoch:02d}.hdf5"
-    bs = 32
+    bs = 16
     checkpoint = ModelCheckpoint(
         filepath,
         monitor='loss',
@@ -101,7 +106,7 @@ def train(model, network_input, network_output):
     )
     callbacks_list = [checkpoint]
 
-    model.fit(network_input, network_output, epochs=200, batch_size=bs, callbacks=callbacks_list)
+    model.fit(network_input, network_output, epochs=10, batch_size=bs, callbacks=callbacks_list)
 
 if __name__ == '__main__':
     train_network()
