2193cf54f832d2ccc9799005318ba1c6c87698cb
Meghana Yadavalli <yadavalli.meghana@gmail.com>
2019-11-06 16:06:08 -0500

    saving notes and outputs to pickle file to prevent reparsing midis

--- ClassifierNet/train.py
+++ ClassifierNet/train.py
@@ -13,33 +13,26 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 
 sequence_length = 100
 
+
 def train_network():
     network_input, network_output = get_notes()
-    model = create_network(network_input)
-    train(model, network_input, network_output)
-
+    with open('objs.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
+        pickle.dump([network_input, network_output], f)
 
 
 def get_notes():
     data = pd.read_csv("song_category.csv")
-
     dic = data.set_index('song_title').T.to_dict('list')
-
     notes = []
-
     network_input = []
     network_output = []
-
     num_notes = 0
 
     for file in glob.glob("midi_songs/*.mid"):
         midi = converter.parse(file)
         song_name = str(file).split("/")[-1]
-
         print("Parsing %s" % file)
-
         notes_to_parse = None
-
         try:
             s2 = instrument.partitionByInstrument(midi)
             notes_to_parse = s2.parts[0].recurse()
@@ -53,14 +46,8 @@ def get_notes():
                 notes.append('.'.join(str(n) for n in element.normalOrder))
 
         pitchnames = sorted(set(item for item in notes))
-
         note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
-
-
         category = dic[song_name][0]
-
-
-
         for i in range(0, len(notes) - sequence_length, 1):
             sequence_in = notes[i:i + sequence_length]
             if category == 'dark':
@@ -71,15 +58,10 @@ def get_notes():
         num_notes += len(notes)
 
     n_patterns = len(network_input)
-
     network_input = numpy.reshape(network_input, (n_patterns, sequence_length))
-
     network_input = network_input / float(num_notes)
-
     network_output = np_utils.to_categorical(network_output)
-
     return (network_input, network_output)
-
 
 
 def create_network(network_input):
@@ -89,9 +71,8 @@ def create_network(network_input):
     model.add(Dense(10, activation='softmax'))
     model.add(Dense(1))
     model.compile(loss='binary_crossentropy', optimizer='rmsprop')
-
-
     return model
+
 
 def train(model, network_input, network_output):
     """ train the neural network """
@@ -108,5 +89,11 @@ def train(model, network_input, network_output):
 
     model.fit(network_input, network_output, epochs=10, batch_size=bs, callbacks=callbacks_list)
 
+
 if __name__ == '__main__':
-    train_network()
+    # train_network()
+    # Getting back the objects:
+    with open('objs.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
+        network_input, network_output = pickle.load(f)
+    model = create_network(network_input)
+    train(model, network_input, network_output)
