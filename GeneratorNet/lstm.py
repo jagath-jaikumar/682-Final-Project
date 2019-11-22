f746db67597463821532c71432c0ea908123e453
Meghana Yadavalli <yadavalli.meghana@gmail.com>
2019-11-21 19:48:21 -0500

    added rough structure of light and dark lstms for hyperparameter tuning

--- GeneratorNet/lstm.py
+++ GeneratorNet/lstm.py
@@ -12,60 +12,20 @@ from tensorflow.keras import utils as np_utils
 from tensorflow.keras.callbacks import ModelCheckpoint
 
 
-def train_network():
-    notes = get_notes()
-    return notes
+# Create Light LSTM
+def light_hyperparameter_tuning():
+    with open('lstm_light.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
+        training_inputs, training_outputs, validation_inputs, validation_outputs = pickle.load(f)
 
-
-def get_notes():
-    notes = []
-
-    for file in glob.glob("midi_songs/*.mid"):
-        midi = converter.parse(file)
-
-        print("Parsing %s" % file)
-
-        notes_to_parse = None
-
-        try:
-            s2 = instrument.partitionByInstrument(midi)
-            notes_to_parse = s2.parts[0].recurse()
-        except:
-            notes_to_parse = midi.flat.notes
-
-        for element in notes_to_parse:
-            if isinstance(element, note.Note):
-                notes.append(str(element.pitch))
-            elif isinstance(element, chord.Chord):
-                notes.append('.'.join(str(n) for n in element.normalOrder))
-
-    with open('data/notes', 'wb') as filepath:
-        pickle.dump(notes, filepath)
-    return notes
-
-
-def prepare_sequences(notes, n_vocab):
-    sequence_length = 100
-    pitchnames = sorted(set(item for item in notes))
-    note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
-    network_input = []
-    network_output = []
-
-    for i in range(0, len(notes) - sequence_length, 1):
-        sequence_in = notes[i:i + sequence_length]
-        sequence_out = notes[i + sequence_length]
-        network_input.append([note_to_int[char] for char in sequence_in])
-        network_output.append(note_to_int[sequence_out])
-
-    n_patterns = len(network_input)
-    network_input = numpy.reshape(network_input, (n_patterns, sequence_length, 1))
-    network_input = network_input / float(n_vocab)
-    network_output = np_utils.to_categorical(network_output)
-    return (network_input, network_output)
-
-
-def create_network(network_input, n_vocab):
-    """ create the structure of the neural network """
+    filepath = "Light-LSTM-improvement-{epoch:02d}.hdf5"
+    checkpoint = ModelCheckpoint(
+        filepath,
+        monitor='loss',
+        verbose=0,
+        save_best_only=True,
+        mode='min'
+    )
+    # nothing above .001, GOOD LR = .00001
     model = Sequential()
     model.add(LSTM(
         512,
@@ -81,33 +41,64 @@ def create_network(network_input, n_vocab):
     model.add(Dense(n_vocab))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
-    return model
+    model.fit(training_inputs, training_outputs, validation_data=(validation_inputs, validation_outputs),
+              epochs=300, batch_size=bs, shuffle=True, verbose=2)  # , callbacks=[checkpoint])
+    # y_pred = model.predict(x_val)
+    # y_pred = np.argmax(y_pred, axis=1)
+    # y_real = np.argmax(y_val, axis=1)
+    # cm = confusion_matrix(y_real, y_pred)
+    # print(cm)
+    # precision = cm[0][0] / (np.sum(cm[0]))
+    # recall = cm[0][0] / (np.sum(cm[0][0] + cm[1][0]))
+    # f_measure = (2*recall*precision)/(recall+precision)
+    # print("Precision = ", precision)
+    # print("Recall = ", recall)
+    # print("f-measure = ", f_measure)
 
 
-def train(model, network_input, network_output):
-    """ train the neural network """
-    filepath = "weights-improvement-{epoch:02d}.hdf5"
-    bs = 64
+# Create Dark LSTM
+def dark_hyperparameter_tuning():
+    with open('lstm_light.pkl', 'rb') as f:
+        training_inputs, training_outputs, validation_inputs, validation_outputs = pickle.load(f)
+
+    filepath = "Light-LSTM-improvement-{epoch:02d}.hdf5"
     checkpoint = ModelCheckpoint(
         filepath,
         monitor='loss',
         verbose=0,
-        save_best_only=False,
+        save_best_only=True,
         mode='min'
     )
-    callbacks_list = [checkpoint]
-    model.fit(network_input, network_output, epochs=200, batch_size=bs, callbacks=callbacks_list)
+    # nothing above .001, GOOD LR = .00001
+    model = Sequential()
+    model.add(LSTM(
+        512,
+        input_shape=(network_input.shape[1], network_input.shape[2]),
+        return_sequences=True
+    ))
+    model.add(Dropout(0.3))
+    model.add(LSTM(512, return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(512))
+    model.add(Dense(256))
+    model.add(Dropout(0.3))
+    model.add(Dense(n_vocab))
+    model.add(Activation('softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+    model.fit(training_inputs, training_outputs, validation_data=(validation_inputs, validation_outputs),
+              epochs=300, batch_size=bs, shuffle=True, verbose=2)  # , callbacks=[checkpoint])
+    # y_pred = model.predict(x_val)
+    # y_pred = np.argmax(y_pred, axis=1)
+    # y_real = np.argmax(y_val, axis=1)
+    # cm = confusion_matrix(y_real, y_pred)
+    # print(cm)
+    # precision = cm[0][0] / (np.sum(cm[0]))
+    # recall = cm[0][0] / (np.sum(cm[0][0] + cm[1][0]))
+    # f_measure = (2*recall*precision)/(recall+precision)
+    # print("Precision = ", precision)
+    # print("Recall = ", recall)
+    # print("f-measure = ", f_measure)
 
-
-# Create Light LSTM
-# Create Dark LSTM
 
 if __name__ == '__main__':
-    # notes = train_network()
-    # Getting back the objects:
-    with open('objs.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
-        network_input, network_output = pickle.load(f)
-    n_vocab = len(set(notes))
-    network_input, network_output = prepare_sequences(notes, n_vocab)
-    model = create_network(network_input, n_vocab)
-    train(model, network_input, network_output)
+    pass
