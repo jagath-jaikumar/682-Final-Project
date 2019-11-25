37a4c8c274ac848955f77f8d89d28a9414069cfe
Jagath Jai Kumar <jagath.jaikumar@gmail.com>
2019-11-25 11:49:28 -0500

    updated lstm py

--- GeneratorNet/lstm.py
+++ GeneratorNet/lstm.py
@@ -5,7 +5,7 @@ from music21 import converter, instrument, note, chord
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dropout
-# from tensorflow.keras.layers import CuDNNLSTM
+from tensorflow.keras.layers import CuDNNLSTM
 from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import Activation
 from tensorflow.keras import utils as np_utils
@@ -13,78 +13,108 @@ from tensorflow.keras.layers import BatchNormalization as BatchNorm
 from tensorflow.keras.callbacks import ModelCheckpoint
 
 
-# Create Light LSTM
-def light_hyperparameter_tuning():
-    with open('lstm_light.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
-        training_inputs, training_outputs, validation_inputs, validation_outputs = pickle.load(f)
-
-    filepath = "Light-LSTM-improvement-{epoch:02d}.hdf5"
-    checkpoint = ModelCheckpoint(
-        filepath,
-        monitor='loss',
-        verbose=0,
-        save_best_only=True,
-        mode='min'
-    )
-    # nothing above .001, GOOD LR = .00001
-    model = Sequential()
-    model.add(LSTM(
-        512,
-        input_shape=(network_input.shape[1], network_input.shape[2]),
-        return_sequences=True
-    ))
-    model.add(Dropout(0.3))
-    model.add(LSTM(512, return_sequences=True))
-    model.add(Dropout(0.3))
-    model.add(LSTM(512))
-    model.add(Dense(256))
-    model.add(Dropout(0.3))
-    model.add(Dense(n_vocab))
-    model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
-    for layer in model.layers:
-        print(layer.output_shape)
-    model.fit(training_inputs, training_outputs, validation_data=(validation_inputs, validation_outputs),
-              epochs=300, batch_size=bs, shuffle=True, verbose=2)  # , callbacks=[checkpoint])
+use_cuda = True
+save_all = True
 
 
-# Create Dark LSTM
-def dark_hyperparameter_tuning():
+def hyperparameter_tuning(feeling):
     with open('notes.pkl', 'rb') as f:
         note_mapping = pickle.load(f)
     n_vocab = len(set(note_mapping))
     print(n_vocab)
-    with open('../Data/lstm_dark_data.pkl', 'rb') as f:
-        training_inputs, training_outputs, validation_inputs, validation_outputs = pickle.load(f)
-    print(training_inputs.shape, training_outputs.shape)
 
-    filepath = "Light-LSTM-improvement-{epoch:02d}.hdf5"
-    checkpoint = ModelCheckpoint(
-        filepath,
-        monitor='loss',
-        verbose=0,
-        save_best_only=True,
-        mode='min'
-    )
+    if feeling == 'dark':
+        with open('../Data/lstm_dark_data.pkl', 'rb') as f:
+            training_inputs, training_outputs, validation_inputs, validation_outputs = pickle.load(f)
+        print(training_inputs.shape, training_outputs.shape)
+    else:
+        with open('../Data/lstm_light.pkl', 'rb') as f:
+            training_inputs, training_outputs, validation_inputs, validation_outputs = pickle.load(f)
+        print(training_inputs.shape, training_outputs.shape)
+
+    filepath = "weights/Light-LSTM-improvement-{epoch:02d}.hdf5"
+    if save_all:
+        checkpoint = ModelCheckpoint(
+            filepath,
+            monitor='loss',
+            verbose=0,
+            save_best_only=False,
+            mode='min'
+        )
+    else:
+        checkpoint = ModelCheckpoint(
+            filepath,
+            monitor='loss',
+            verbose=0,
+            save_best_only=True,
+            mode='min'
+        )
     model = Sequential()
-    model.add(LSTM(512, input_shape=(training_inputs.shape[1], training_inputs.shape[2]),
-                   recurrent_dropout=0.3, return_sequences=True))
-    model.add(LSTM(512, return_sequences=True, recurrent_dropout=0.3, ))
-    model.add(LSTM(512))
+    if use_cuda:
+        model.add(CuDNNLSTM(512, input_shape=(training_inputs.shape[1], training_inputs.shape[2]),
+                        return_sequences=True))
+        model.add(CuDNNLSTM(512, return_sequences=True, ))
+        model.add(CuDNNLSTM(512))
+    else:
+        model.add(LSTM(512, input_shape=(training_inputs.shape[1], training_inputs.shape[2]),
+                       recurrent_dropout=0.3, return_sequences=True))
+        model.add(LSTM(512, return_sequences=True, recurrent_dropout=0.3, ))
+        model.add(LSTM(512))
     model.add(BatchNorm())
     model.add(Dropout(0.3))
     model.add(Dense(256))
     model.add(Activation('relu'))
     model.add(BatchNorm())
     model.add(Dropout(0.3))
-    model.add(Dense(n_vocab))
+    model.add(Dense(99)) # n_vocab
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
     for layer in model.layers:
         print(layer.output_shape)
-    model.fit(training_inputs, training_outputs, validation_data=(validation_inputs, validation_outputs),
-              epochs=150, batch_size=128, shuffle=True, verbose=1)  # , callbacks=[checkpoint])
+    if save_all:
+        model.fit(training_inputs, training_outputs, validation_data=(validation_inputs, validation_outputs),
+                  epochs=150, batch_size=128, shuffle=True, verbose=1, callbacks=[checkpoint])
+    else:
+        model.fit(training_inputs, training_outputs, validation_data=(validation_inputs, validation_outputs),
+                  epochs=150, batch_size=128, shuffle=True, verbose=1) #, callbacks=[checkpoint])
 
 
 if __name__ == '__main__':
-    dark_hyperparameter_tuning()
+    hyperparameter_tuning(feeling="dark")
+
+
+
+
+# Create Light LSTM
+# def light_hyperparameter_tuning():
+#     with open('lstm_light.pkl', 'rb') as f:  # Python 3: open(..., 'rb')
+#         training_inputs, training_outputs, validation_inputs, validation_outputs = pickle.load(f)
+#
+#     filepath = "Light-LSTM-improvement-{epoch:02d}.hdf5"
+#     checkpoint = ModelCheckpoint(
+#         filepath,
+#         monitor='loss',
+#         verbose=0,
+#         save_best_only=True,
+#         mode='min'
+#     )
+#     # nothing above .001, GOOD LR = .00001
+#     model = Sequential()
+#     model.add(LSTM(
+#         512,
+#         input_shape=(network_input.shape[1], network_input.shape[2]),
+#         return_sequences=True
+#     ))
+#     model.add(Dropout(0.3))
+#     model.add(LSTM(512, return_sequences=True))
+#     model.add(Dropout(0.3))
+#     model.add(LSTM(512))
+#     model.add(Dense(256))
+#     model.add(Dropout(0.3))
+#     model.add(Dense(n_vocab))
+#     model.add(Activation('softmax'))
+#     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+#     for layer in model.layers:
+#         print(layer.output_shape)
+#     model.fit(training_inputs, training_outputs, validation_data=(validation_inputs, validation_outputs),
+#               epochs=300, batch_size=bs, shuffle=True, verbose=2)  # , callbacks=[checkpoint])
--- GeneratorNet/predict.py
+++ GeneratorNet/predict.py
@@ -6,24 +6,29 @@ from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import BatchNormalization as BatchNorm
+
+sequence_length = 100
+
 
 def generate():
     with open('data/notes', 'rb') as filepath:
         notes = pickle.load(filepath)
-
+    print(notes)
     pitchnames = sorted(set(item for item in notes))
     n_vocab = len(set(notes))
 
     network_input, normalized_input = prepare_sequences(notes, pitchnames, n_vocab)
     model = create_network(normalized_input, n_vocab)
     prediction_output = generate_notes(model, network_input, pitchnames, n_vocab)
+    print(prediction_output)
     create_midi(prediction_output)
 
 def prepare_sequences(notes, pitchnames, n_vocab):
 
     note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
 
-    sequence_length = 100
+
     network_input = []
     output = []
     for i in range(0, len(notes) - sequence_length, 1):
@@ -40,47 +45,55 @@ def prepare_sequences(notes, pitchnames, n_vocab):
 
     return (network_input, normalized_input)
 
-def create_network(network_input, n_vocab):
+def create_network(training_inputs, n_vocab):
     model = Sequential()
-    model.add(LSTM(
-        512,
-        input_shape=(network_input.shape[1], network_input.shape[2]),
-        return_sequences=True
-    ))
-    model.add(Dropout(0.3))
-    model.add(LSTM(512, return_sequences=True))
-    model.add(Dropout(0.3))
+    model.add(LSTM(512, input_shape=(training_inputs.shape[1], training_inputs.shape[2]),
+                   recurrent_dropout=0.3, return_sequences=True))
+    model.add(LSTM(512, return_sequences=True, recurrent_dropout=0.3, ))
     model.add(LSTM(512))
-    model.add(Dense(256))
+    model.add(BatchNorm())
     model.add(Dropout(0.3))
-    model.add(Dense(n_vocab))
+    model.add(Dense(256))
+    model.add(Activation('relu'))
+    model.add(BatchNorm())
+    model.add(Dropout(0.3))
+    model.add(Dense(353)) # should be n_vocab but is wrong dimension?
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
-    model.load_weights('weights-improvement-104.hdf5')
+    model.load_weights('dark-LSTM-improvement-150.hdf5')
 
     return model
 
 def generate_notes(model, network_input, pitchnames, n_vocab):
     start = numpy.random.randint(0, len(network_input)-1)
-
+    print("pitchnamnes")
+    print(pitchnames)
     int_to_note = dict((number, note) for number, note in enumerate(pitchnames))
 
+
+    print('int to note')
+    print(int_to_note)
     pattern = network_input[start]
     prediction_output = []
-
-    for note_index in range(500):
+    prev_prediction = 0
+    for note_index in range(5):
         prediction_input = numpy.reshape(pattern, (1, len(pattern), 1))
         prediction_input = prediction_input / float(n_vocab)
 
+
         prediction = model.predict(prediction_input, verbose=0)
+        print(numpy.sum(prediction))
 
         index = numpy.argmax(prediction)
+        print(index)
         result = int_to_note[index]
+        print(result)
         prediction_output.append(result)
 
         pattern.append(index)
         pattern = pattern[1:len(pattern)]
+        prev_prediction = prediction
 
     return prediction_output
 
