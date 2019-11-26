23a40f6c28a5f80e3b90822184a241c32345a565
Jagath Jai Kumar <jagath.jaikumar@gmail.com>
2019-11-25 22:55:59 -0500

    fixing bugs

--- GeneratorNet/predict.py
+++ GeneratorNet/predict.py
@@ -1,6 +1,5 @@
 import pickle
-import numpy as np
-import sys
+import numpy
 from music21 import instrument, note, stream, chord
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
@@ -14,32 +13,30 @@ use_cuda = True
 if use_cuda:
     from tensorflow.keras.layers import CuDNNLSTM
 
-
 def generate(feeling):
-    with open('../Data/notes.pkl', 'rb') as f:
-        all_notes, ps_to_int, int_to_ps, ps_to_note_name = pickle.load(f)
-    # print(all_notes)
-    # print(ps_to_int)
-    # print(int_to_ps)
-    # print(ps_to_note_name)
+    with open('../Data/notes.pkl', 'rb') as filepath:
+        notes = pickle.load(filepath)
+    notes = notes[0]
 
-    network_input = prepare_input_sequences(all_notes)
-    model = create_network(network_input, len(all_notes), feeling)
-    prediction_output = generate_notes(model, network_input, int_to_ps, ps_to_note_name)
+    network_input = prepare_sequences(notes)
+
+    model = create_network(network_input, feeling)
+
+    prediction_output = generate_notes(model, network_input, notes)
+
     create_midi(prediction_output)
 
-
-def prepare_input_sequences(all_notes):
+def prepare_sequences(notes):
     network_input = []
 
     for i in range(sequence_length):
-        sequence_in = all_notes[np.random.randint(0, len(all_notes))]
+        sequence_in = notes[numpy.random.randint(0, len(notes)-1)]
         network_input.append(sequence_in)
 
+    network_input = numpy.reshape(network_input,(1,sequence_length,1))
     return network_input
 
-
-def create_network(training_inputs, n_vocab, feeling):
+def create_network(training_inputs,feeling):
     model = Sequential()
     if use_cuda:
         model.add(CuDNNLSTM(512, input_shape=(training_inputs.shape[1], training_inputs.shape[2]),
@@ -57,7 +54,7 @@ def create_network(training_inputs, n_vocab, feeling):
     model.add(Activation('relu'))
     model.add(BatchNorm())
     model.add(Dropout(0.3))
-    model.add(Dense(n_vocab))
+    model.add(Dense(99)) # should be n_vocab but is wrong dimension?
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
@@ -67,23 +64,18 @@ def create_network(training_inputs, n_vocab, feeling):
         model.load_weights('light-LSTM-improvement-150.hdf5')
     return model
 
+def generate_notes(model, network_input,notes):
 
-def generate_notes(model, network_input, notes, int_to_ps, ps_to_note_name):
     prediction_output = []
+
     pattern = network_input
-
     for new_note in range(5):
-        new_prediction = model.predict(pattern, verbose=0)
-        index = np.argmax(new_prediction)
-
-        # Adding predicted note to generate the next input
-        ps_value = int_to_ps[index]
-        pattern.append(ps_value)
+        new_prediction = model.predict(pattern, verbose = 0)
+        index = numpy.argmax(new_prediction)
+        result = notes[index]
+        prediction_output.append(result)
+        pattern.append(result)
         pattern = pattern[1:]
-
-        # Adding note to prediction output
-        note_name = ps_to_note_name[ps_value]
-        prediction_output.append(note_name)
 
     return prediction_output
 
@@ -93,16 +85,27 @@ def create_midi(prediction_output):
     output_notes = []
 
     for pattern in prediction_output:
-        # We don't have chords so we only need to go by notes
-        new_note = note.Note(pattern)
-        new_note.offset = offset
-        new_note.storedInstrument = instrument.Piano()
-        output_notes.append(new_note)
+        if ('.' in pattern) or pattern.isdigit():
+            notes_in_chord = pattern.split('.')
+            notes = []
+            for current_note in notes_in_chord:
+                new_note = note.Note(int(current_note))
+                new_note.storedInstrument = instrument.Piano()
+                notes.append(new_note)
+            new_chord = chord.Chord(notes)
+            new_chord.offset = offset
+            output_notes.append(new_chord)
+        else:
+            new_note = note.Note(pattern)
+            new_note.offset = offset
+            new_note.storedInstrument = instrument.Piano()
+            output_notes.append(new_note)
+
         offset += 0.5
 
     midi_stream = stream.Stream(output_notes)
-    midi_stream.write('midi', fp='test_output.mid')
 
+    midi_stream.write('midi', fp='test_output.mid')
 
 if __name__ == '__main__':
     generate(feeling='dark')
