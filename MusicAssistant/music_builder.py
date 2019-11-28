2c2107cf8829af149b46618fe9c449bc3cdcdb53
Jagath Jai Kumar <jagath.jaikumar@gmail.com>
2019-11-27 22:43:01 -0500

    back to working version

--- MusicAssistant/music_builder.py
+++ MusicAssistant/music_builder.py
@@ -1,68 +1,63 @@
 import pickle
-import numpy as np
+import numpy
 from GeneratorNet import predict as GenNet
 from ClassifierNet import predict as ClassNet
 
 sequence_length = 100
 
-
 def initialize_all():
-    with open('../Data/notes.pkl', 'rb') as f:
+    with open('Data/notes.pkl', 'rb') as f:
         all_notes, ps_to_int, int_to_ps, ps_to_note_name = pickle.load(f)
-    network_input_flat, network_input_shaped = GenNet.prepare_input_sequence(all_notes)
+    network_input_flat, network_input_shaped = GenNet.prepare_sequences(all_notes)
     # Create 2 networks, 1 dark 1 light so we don't have to keep reloading the weights
     light_generator = GenNet.create_network(network_input_shaped, "light", len(all_notes))
     dark_generator = GenNet.create_network(network_input_shaped, "dark", len(all_notes))
     classifier = ClassNet.create_network()
-    return light_generator, dark_generator, classifier, network_input_shaped
+    return light_generator, dark_generator, classifier, network_input_shaped, all_notes
 
 
-def run_models(light_generator, dark_generator, classifier, network_input, song_length):
-    with open('../Data/notes.pkl', 'rb') as f:
+def run_models(light_generator, dark_generator, classifier, network_input, song_length, all_notes):
+    with open('Data/notes.pkl', 'rb') as f:
         all_notes, ps_to_int, int_to_ps, ps_to_note_name = pickle.load(f)
     full_output = []
     curr_input = network_input
     moods = []
-    query_interval = 20# , 50
-    j = 1
-    i = 0
-    while i < song_length:
-        class_input = np.reshape(curr_input, (1, sequence_length))
-        print("classifier")
-        print(class_input.shape)
+    for i in range(song_length):
+
+        class_input = numpy.reshape(curr_input,(1,sequence_length))
         if classifier.predict(class_input) == "dark":
-            print("predicted dark")
-            while j % query_interval != 0:
-                note_name, pattern = GenNet.generate_next_note(dark_generator, curr_input, int_to_ps, ps_to_note_name)
-                full_output.append(note_name)
-                curr_input = list(curr_input.flatten())
-                curr_input.append(pattern)
-                curr_input = curr_input[-100:]
-                curr_input = np.reshape(curr_input, (1, 100, 1))
-                j += 1
-                i += 1
+            next_notes = GenNet.generate_notes(dark_generator,curr_input,all_notes, phrase_length = 100)
+            moods.append('dark')
         else:
-            print("predicted light")
-            while j % query_interval != 0:
-                print(type(curr_input), j)
-                note_name, pattern = GenNet.generate_next_note(light_generator, curr_input, int_to_ps, ps_to_note_name)
-                full_output.append(note_name)
-                curr_input = list(curr_input.flatten())
-                curr_input.append(pattern)
-                curr_input = curr_input[-100:]
-                curr_input = np.reshape(curr_input, (1, 100, 1))
-                j += 1
-                i += 1
+            next_notes = GenNet.generate_notes(light_generator,curr_input,all_notes, phrase_length = 100)
+            moods.append('light')
+
+        for note in next_notes:
+            full_output.append(note)
+
         print("done with iteration ", i+1)
+        curr_input = full_output[-100:]
+
+        # this is my change
+
     return full_output, moods
 
 
 if __name__ == '__main__':
     for i in range(5):
-        light_generator, dark_generator, classifier, network_input = initialize_all()
-        song_length = 100
-        generated_song, moods = run_models(light_generator, dark_generator, classifier, network_input, song_length)
+        light_generator, dark_generator, classifier, network_input, all_notes = initialize_all()
+        song_length = 5
+        generated_song, moods = run_models(light_generator, dark_generator, classifier, network_input, song_length, all_notes)
+        dcount, lcount = 0,0
+        for j in moods:
+            if j == 'dark':
+                dcount+=1
+            else:
+                lcount+=1
         filename = "GeneratedSongs/"
-        filename += 'Mixed' + str(i)
+        if dcount > lcount:
+            filename+="Dark" + str(i+1)
+        else:
+            filename+='Light'+ str(i+1)
 
         GenNet.create_midi(generated_song, filename)
